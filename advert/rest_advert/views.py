a174bee8ab36fed2fd644da38c99352ee0fae4b5
alexyak1 <yakimchuk.nstu@gmail.com>
2021-06-28 21:30:53 +0200

    add sorting by created and price fields

--- advert/rest_advert/views.py
+++ advert/rest_advert/views.py
@@ -1,11 +1,14 @@
 from rest_advert.models import Ad
 from rest_advert.serializers import AdSerializer
 from rest_framework import generics
+from rest_framework import filters
 
 
 class AdList(generics.ListCreateAPIView):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['created', 'price']
 
 
 class AdDetail(generics.RetrieveUpdateDestroyAPIView):

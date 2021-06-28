54f17d4a46e66ecd9a437ff992651e80e3743ea6
alexyak1 <yakimchuk.nstu@gmail.com>
2021-06-28 13:13:23 +0200

    rewrite views to object oriented style

--- advert/rest_advert/urls.py
+++ advert/rest_advert/urls.py
@@ -1,7 +1,18 @@
+# from django.urls import path
+# from rest_advert import views
+
+# urlpatterns = [
+#     path('ads/', views.ad_list),
+#     path('ads/<int:pk>/', views.ad_detail),
+# ]
+
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from rest_advert import views
 
 urlpatterns = [
-    path('ads/', views.ad_list),
-    path('ads/<int:pk>/', views.ad_detail),
+    path('ads/', views.AdList.as_view()),
+    path('ads/<int:pk>/', views.AdDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- advert/rest_advert/views.py
+++ advert/rest_advert/views.py
@@ -1,51 +1,52 @@
-# from django.shortcuts import render
-
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from rest_advert.models import Ad
 from rest_advert.serializers import AdSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-@csrf_exempt
-def ad_list(request):
+
+class AdList(APIView):
     """
-    List all code ads, or create a new ad.
+    List all ads, or create a new ad.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         ads = Ad.objects.all()
         serializer = AdSerializer(ads, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = AdSerializer(data=data)
+    def post(self, request, format=None):
+        serializer = AdSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def ad_detail(request, pk):
+class AdDetail(APIView):
     """
-    Retrieve, update or delete a code ad.
+    Retrieve, update or delete a ad instance.
     """
-    try:
-        ad = Ad.objects.get(pk=pk)
-    except Ad.DoesNotExist:
-        return HttpResponse(status=404)
+    def get_object(self, pk):
+        try:
+            return Ad.objects.get(pk=pk)
+        except Ad.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
-        serializer = AdSerializer(ad)
-        return JsonResponse(serializer.data)
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = AdSerializer(snippet)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = AdSerializer(ad, data=data)
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = AdSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        ad.delete()
-        return HttpResponse(status=204)
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+

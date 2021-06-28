d52769a66b542d87534eacad27c7e4095f7c1c39
alexyak1 <yakimchuk.nstu@gmail.com>
2021-06-28 14:21:28 +0200

    update readme

--- advert/rest_advert/urls.py
+++ advert/rest_advert/urls.py
@@ -1,11 +1,3 @@
-# from django.urls import path
-# from rest_advert import views
-
-# urlpatterns = [
-#     path('ads/', views.ad_list),
-#     path('ads/<int:pk>/', views.ad_detail),
-# ]
-
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_advert import views
